2bbd24d219ef80a3d0cb01df68ef26e8834fc6a2
Tony Lee <tloeney1785@gmail.com>
2020-06-16 20:09:59 -0700

    created tokenise.py
    able to pull data from twitter with # filter but tokenise cannot open .json file atm

--- tweepy_project.py
+++ tweepy_project.py
@@ -1,5 +1,8 @@
 import tweepy
 from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+import json
 
 consumer_key = '7GyJCGrUdowLP5yJ0fJBZVW29'
 consumer_secret = 'kChU2Xx8sbrEhW52vd5cHn7wVHHYhCoVbzXIxobZv1nI3cIRQ0'
@@ -11,6 +14,24 @@ auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 
-for status in tweepy.Cursor(api.home_timeline).items(10):
-    # Process a single status
-    print(status.text)
+# def process_or_store(tweet):
+#     print(json.dumps(tweet))
+ 
+class MyListener(StreamListener):
+ 
+    def on_data(self, data):
+        try:
+            with open('python.json', 'a') as f:
+                f.write(data)
+                return True
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
+        return True
+ 
+    def on_error(self, status):
+        print(status)
+        return True
+ 
+twitter_stream = Stream(auth, MyListener())
+twitter_stream.filter(track=['#BLM'])
+

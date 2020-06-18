1259c85cb4046ccc4add66fa8e8b1e899fb19c5d
Tony Lee <tloeney1785@gmail.com>
2020-06-18 15:48:47 -0700

    implemented time limit

--- tweepy_project.py
+++ tweepy_project.py
@@ -3,6 +3,7 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy.streaming import StreamListener
 import json
+import time
 
 consumer_key = '7GyJCGrUdowLP5yJ0fJBZVW29'
 consumer_secret = 'kChU2Xx8sbrEhW52vd5cHn7wVHHYhCoVbzXIxobZv1nI3cIRQ0'
@@ -16,18 +17,32 @@ api = tweepy.API(auth)
 
 class MyListener(StreamListener):
 
+    def __init__(self, time_limit=60):
+        self.start_time = time.time()
+        self.limit = time_limit
+        self.saveFile = open('python.json', 'a')
+        super(MyListener, self).__init__()
+
     def on_data(self, data):
-        try:
-            with open('python.json', 'a') as f:
-                f.write(data)
-                return True
-        except BaseException as e:
-            print("Error on_data: %s" % str(e))
-        return True
+
+        if (time.time() - self.start_time) < self.limit:
+            self.saveFile.write(data)
+            self.saveFile.write('\n')
+            return True
+        else:
+            self.saveFile.close()
+            return False
+        # try:
+        #     with open('python.json', 'a') as f:
+        #         f.write(data)
+        #         return True
+        # except BaseException as e:
+        #     print("Error on_data: %s" % str(e))
+        # return True
  
     def on_error(self, status):
         print(status)
         return True
  
-twitter_stream = Stream(auth, MyListener())
+twitter_stream = Stream(auth, listener=MyListener(time_limit=10))
 twitter_stream.filter(track=['#BLM'], is_async=True)

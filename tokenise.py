18e18f2e90abafbd235cda6fe645d9d0af3a3aa8
Tony Lee <tloeney1785@gmail.com>
2020-06-17 17:13:43 -0700

    implented time chart

--- tokenise.py
+++ tokenise.py
@@ -7,6 +7,7 @@ from collections import defaultdict
 from nltk.corpus import stopwords
 import string
 import vincent
+import pandas
  
 emoticons_str = r"""
     (?:
@@ -45,22 +46,49 @@ def preprocess(s, lowercase=False):
     return tokens
 
 fname = 'python.json'
+# with open(fname, 'r') as f:
+#     count_all = Counter()
+#     for line in f:
+#         try:
+#             tweet = json.loads(line)
+#             # Count hashtags only
+#             terms_hash = [term for term in preprocess(tweet['text']) if term.startswith('#')] 
+#             count_all.update(terms_hash)
+#             # terms_stop = [term for term in preprocess(tweet['text']) if term not in stop]
+#         except ValueError:
+#             pass
+#     # Print the first 5 most frequen
+#     print(count_all.most_common(5))
+
+dates_BLM = []
+# f is the file pointer to the JSON data set
 with open(fname, 'r') as f:
-    count_all = Counter()
     for line in f:
         try:
             tweet = json.loads(line)
-            # Count hashtags only
-            terms_hash = [term for term in preprocess(tweet['text']) if term.startswith('#')] 
-            count_all.update(terms_hash)
-            # terms_stop = [term for term in preprocess(tweet['text']) if term not in stop]
+            # let's focus on hashtags only at the moment
+            terms_hash = [term for term in preprocess(tweet['text']) if term.startswith('#')]
+            # track when the hashtag is mentioned
+            if '#BLM' in terms_hash:
+                dates_BLM.append(tweet['created_at'])
         except ValueError:
             pass
-    # Print the first 5 most frequen
-    print(count_all.most_common(5))
+ 
+# a list of "1" to count the hashtags
+ones = [1]*len(dates_BLM)
+# the index of the series
+idx = pandas.DatetimeIndex(dates_BLM)
+# the actual series (at series of 1s for the moment)
+ITAvWAL = pandas.Series(ones, index=idx)
+ 
+# Resampling / bucketing
+per_minute = ITAvWAL.resample('10S').sum().fillna(0)
 
-word_freq = count_all.most_common(5)
-labels, freq = zip(*word_freq)
-data = {'data': freq, 'x': labels}
-bar = vincent.Bar(data, iter_idx='x')
-bar.to_json('term_freq.json')
+# word_freq = count_all.most_common(5)
+# labels, freq = zip(*word_freq)
+# data = {'data': freq, 'x': labels}
+# bar = vincent.Bar(data, iter_idx='x')
+# bar.to_json('term_freq.json')
+time_chart = vincent.Line(per_minute)
+time_chart.axis_titles(x='Time', y='Freq')
+time_chart.to_json('time_chart.json')
